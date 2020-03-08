999e407160b923349439f53a794ae62b7778f62e
enesuzun2002 <enesuzun200227@gmail.com>
2020-03-08 21:17:40 +0100

    zerofltexx: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_zerofltexx.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_zerofltexx-user \
+    lineage_zerofltexx-userdebug \
+    lineage_zerofltexx-eng
